09c03651bc744ceeaf3ad0400ae4a3a8bebae114
Che J Holloway <chejholloway@outlook.com>
2023-06-23 10:35:28 -0700

    chore(global): :sparkles: initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
 import './App.css';
 
 function App() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+
+   const fetchData = () => {
+      fetch("https://wgg522pwivhvi5gqsn675gth3q0otdja.lambda-url.us-east-1.on.aws/70726f")
+        .then((response) => {
+           let html = response.text();
+           return html;
+        })
+        .then((data) => {
+           setData(data);
+           console.log("DATA:  " , data);           
+        })
+        .catch((err) => { 
+           setError(error);
+           console.log('Fetch Error', err);  
+        })
+        .finally(() => {
+           setLoading(false);
+         });        
+
+    }
+
+    fetchData();
+  }, [error, data]);
+  
+  
+
+  if (loading) return "Loading...";
+  if (error) return "Error!";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <pre>{JSON.stringify(data)}</pre>
+    </>
+
+
+    
   );
 }
 
 export default App;
+
